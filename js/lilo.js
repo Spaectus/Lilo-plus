64d68e0b3fd9394a881f77fee156eb6339bd964c
Spaectus <30037190+Spaectus@users.noreply.github.com>
2022-11-25 21:15:38 +0100

    automatic redirection to startpage when lilo is not available

--- js/lilo.js
+++ js/lilo.js
@@ -1,4 +1,11 @@
 function main_liloplus() {
+  const div_error = document.querySelector("div.hidden>div.cf-error-details")
+  if (div_error!==null){
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    window.location.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(urlParams.get("q"))
+    return null
+  }
   const f=document.querySelector("#search-tab-btn__google")
   f.title = f.title.replaceAll("Google", "Startpage")
   f.innerText = "Startpage"
@@ -6,6 +13,8 @@ function main_liloplus() {
     window.location.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(document.querySelector("#lilo-search").value);
   };
 
+
+
 }
-console.log("Lilo+");
+//console.log("Lilo+");
 main_liloplus();
