e5a8604e055e15e66b3bbdaffe233d8a635a8fe7
Spaectus <30037190+Spaectus@users.noreply.github.com>
2023-10-23 23:06:36 +0200

    new matches in manifest

--- js/lilo.js
+++ js/lilo.js
@@ -1,9 +1,9 @@
 function main_liloplus() {
   const search = document.querySelector('input[name="q"]').value;
-  const a = document.querySelector('#tabs>a[href^="https://google."]')
+  const a = document.querySelector('#tabs>a[href^="https://google."]');
   a.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(search);
-  a.querySelector("span").innerText = "Startpage"
-  
+  a.querySelector("span").innerText = "Startpage";
+
 }
 //console.log("Lilo+");
 main_liloplus();
