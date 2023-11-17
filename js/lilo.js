0a5fe139a80b2cb889d8c60cbb2bb063aca7802f
Spaectus <30037190+Spaectus@users.noreply.github.com>
2023-11-17 14:01:11 +0000

    Update lilo.js

--- js/lilo.js
+++ js/lilo.js
@@ -1,8 +1,18 @@
 function main_liloplus() {
   const search = document.querySelector('input[name="q"]').value;
   const a = document.querySelector('#tabs>a[href^="https://google."]');
-  a.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(search);
-  a.querySelector("span").innerText = "Startpage";
+  if (a!=null) {
+    // new version of lilo
+    a.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(search);
+    a.querySelector("span").innerText = "Startpage";
+  } else {
+    const f = document.querySelector("#search-tab-btn__google")
+    f.title = f.title.replaceAll("Google", "Startpage")
+    f.innerText = "Startpage"
+    f.onclick = function(){
+      window.location.href = "https://www.startpage.com/do/dsearch?query="+encodeURIComponent(document.querySelector("#lilo-search").value);
+    };
+  }
 
 }
 //console.log("Lilo+");
